7849b8238d30d0fddee271bbea14186a2a98df4d
Eliot <halychprogramming@gmail.com>
2023-05-08 23:02:14 +0200

    Take a quick note on data types in JS

---
+++ foundations/notes/4_javascript_basics/2_fundamentals_2/datatypes.js
@@ -0,0 +1,23 @@
+// There are 8 basic data types in JavaScript.
+
+// Seven primitive data types:
+// "number" for numbers of any kind: integer or floating-point(integers are limited by ±(2^53-1), which is roughly 9*10^12).
+// "bigint" for integer numbers of arbitrary length.
+// "string" for strings. A string may have zero or more characters, there’s no separate single-character type.
+// "boolean" for true/false.
+// "null" for unknown values – a standalone type that has a single value null.
+// "undefined" for unassigned values – a standalone type that has a single value undefined.
+// "symbol" for unique identifiers.
+
+// And one non-primitive data type:
+// "object" for more complex data structures.
+
+
+
+// The "typeof" operator allows us to see which type is stored in a variable.
+ 
+// Usually used as typeof x, but typeof(x) is also possible.
+// Returns a string with the name of the type, like "string".
+// For null returns "object" – this is an error in the language, it’s not actually an object.
+ 
+// In the next chapters, we’ll concentrate on primitive values and once we’re familiar with them, we’ll move on to objects.
