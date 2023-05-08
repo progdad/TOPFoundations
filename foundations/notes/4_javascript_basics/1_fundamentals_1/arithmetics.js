8ca3d7c98a2ba752fd96aa0dbfce2b4938ab9466
Eliot <halychprogramming@gmail.com>
2023-05-08 19:30:12 +0200

    Finish the first JS lesson

---
+++ foundations/notes/4_javascript_basics/1_fundamentals_1/arithmetics.js
@@ -0,0 +1,29 @@
+
+// Everything is pretty much the same as in python.
+let x = 5;
+let y = 2;
+let z = x / y;
+console.log(z);
+
+let k = (100 + 50) * 3;
+console.log(k) // 450
+
+
+
+// Incrementing, Decrementing
+let num = 5;
+num++;
+console.log(num); // 6
+
+num--;
+num--;
+console.log(num); // 4
+
+let b = 7 * num;
+console.log(b);
+
+// Just as in Python, there are syntax sugars like:
+let myNum = 3;
+myNum += 2;
+myNum **= 3;
+console.log(myNum); // 125
